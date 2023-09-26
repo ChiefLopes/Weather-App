6c9261be65ccde8486a630a5c41b1026d1ce3849
ChiefLopes <chieflopesekede@gmail.com>
2023-09-26 16:48:37 +0100

    added the rain image along with styles

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -9,10 +9,19 @@ const Weather = () => {
         <span className="search">
           <Icons.search className="search-icon" />
         </span>
-          </div>
-          <div>
-            <Icons.rain className="rain" />
-          </div>
+      </div>
+      <div>
+        <Icons.rain className="rain" />
+      </div>
+      <div className="weather-desc">
+        <span className="weather-temp">23°c</span>
+              <div className="weather-location">London</div>
+              <div className="data-container">
+                  <div className="element">
+                      <img src="" alt="icon" />
+                  </div>
+              </div>
+      </div>
     </div>
   );
 };
